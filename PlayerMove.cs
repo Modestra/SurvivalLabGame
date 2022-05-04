51f171c85f4b107c67df581a3115c3fd740f12b2
Kirill <104451159+Modestra@users.noreply.github.com>
2022-05-05 01:00:34 +0700

    Start Position

---
+++ PlayerMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
