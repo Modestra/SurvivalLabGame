f5671d90e126a9b14d8f60c99f39e70c09aa93f2
Kirill <104451159+Modestra@users.noreply.github.com>
2022-05-05 10:12:43 +0700

    05.05.2022

--- PlayerMove.cs
+++ PlayerMove.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    
+    public GameObject player;
+    public Camera cam;
     void Start()
     {
-        
+       
     }
 
     
     void Update()
     {
-        
+        getMoveObject();
+    }
+    public void getMoveObject()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+                
     }
 }
