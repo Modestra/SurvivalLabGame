88da72d81ca20f748245c2b77c0640e91ff6d41b
Kirill <104451159+Modestra@users.noreply.github.com>
2022-06-22 12:28:48 +0700

    22.06.2022

--- HeadController.cs
+++ HeadController.cs
@@ -6,6 +6,7 @@ public class HeadController : MonoBehaviour
 {
     public float xPos, yPos;
     public GameObject head;
+    private float horizontal, vertical;
     void Start()
     {
         
@@ -13,9 +14,25 @@ public class HeadController : MonoBehaviour
 
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
         xPos += Input.GetAxis("Mouse X");
         yPos += Input.GetAxis("Mouse Y");
         head.transform.rotation = Quaternion.Euler(-yPos,xPos,0);
         transform.rotation = Quaternion.Euler(-yPos, xPos, 0);
+        transform.Translate(Vector3.forward * Time.deltaTime * 10 * vertical);
+        transform.Rotate(Vector3.up, Time.deltaTime * 10 * horizontal);
+    }
+}
+public class PlayerTouch
+{
+    Ray ray;
+    RaycastHit hit;
+    public void MouseTouch()
+    {
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.DrawLine(Input.mousePosition, Vector3.forward*1000, Color.red);
+        }
     }
 }
